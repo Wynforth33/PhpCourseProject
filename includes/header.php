a130a097ed78c3743e9aa2a1e75050acda71f1eb
Wynforth33 <34152920+Wynforth33@users.noreply.github.com>
2018-11-02 17:38:57 -0500

    Template Header/Footer | Add store-hours.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,29 @@
+<?php 
+  $companyName = "Franklin's Fine Dining";
+  include('includes/arrays.php');
+?>
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Code Dynamic Websites with PHP</title>
+    <link rel="stylesheet" href="styles/styles.css">
+  </head>
+    
+  <body id="final-example">
+
+    <div class="wrapper">
+
+      <div id="banner">
+        <a href="/" title="Return to Home">
+          <img src="img/banner.png" alt="Franklin's Fine Dining" />
+        </a>
+      </div><!-- .banner -->
+
+      <div id="nav">
+        <?php include('includes/nav.php'); ?>
+      </div><!-- nav -->
+
+      <div class="content">
