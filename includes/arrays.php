b54e93c6e71d045a0ec93e3c070bf087ac29c99f
Wynforth33 <34152920+Wynforth33@users.noreply.github.com>
2018-11-05 17:30:43 -0600

    Create Menu Array/Template

--- includes/arrays.php
+++ includes/arrays.php
@@ -2,18 +2,22 @@
   
   // Nav Menu Items
   $navItems = array(
+      
                   array(
                       'slug'  => "index.php",
                       'title' => "Home"
                   ),
+      
                   array(
                       'slug'  => "team.php",
                       'title' => "Team"
                   ),
+      
                   array(
                       'slug'  => "menu.php",
                       'title' => "Menu"
                   ),
+      
                   array(
                       'slug'  => "contact.php",
                       'title' => "Contact"
@@ -22,24 +26,61 @@
 
   // Team Members
   $teamMembers = array(
+      
                      array(
                          'name'     => "Frankie III",
                          'position' => "Owner",
-                         'bio'      => "Frankie is the great-grandson of the original                    Franklin. He is the owner of Franklin's Fine                      Dining. He cooks a mean Fritata!",
+                         'bio'      => "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a mean Fritata!",
                          'img'      => "frankie"
                      ),
+      
                      array(
                          'name'     => "Francis",
                          'position' => "General Manager",
-                         'bio'      => "Francis knows her stuff. The big sister of Frankie                himself, she runs the show. Don't miss her                        Margherita Mondays!",
+                         'bio'      => "Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don't miss her Margherita Mondays!",
                          'img'      => "francis"
                      ),
+      
                      array(
                          'name'     => "Carlos",
                          'position' => "Head Chef",
-                         'bio'      => "Carlos is the epitome of the phrase &ldquo;Don't                  judge a book by it's cover&rdquo; &mdash; You                    simply cannot find a better chef.",
+                         'bio'      => "Carlos is the epitome of the phrase &ldquo;Don't judge a book by it's cover&rdquo; &mdash; You simply cannot find a better chef.",
                          'img'      => "carlos"
                      ),
                  );
+
+  // Menu Items
+  $menuItems = array(
+      
+                   "club-sandwhich" => array(
+                       'title' => "Club Sandwhich",
+                       'price' => 11,
+                       'blurb' => "Bacon ipsum dolor amet cow swine t-bone kevin bacon. Pig short ribs ground round andouille brisket strip steak. Ball tip filet mignon drumstick doner short loin chuck prosciutto sirloin strip steak burgdoggen chicken shankle kielbasa.",
+                       'drink' => "Club Soda"
+                   ),
+      
+                   "dill-salmon" => array(
+                       'title' => "Lemon &amp; Dill Salmon",
+                       'price' => 18,
+                       'blurb' => "Bacon ipsum dolor amet cow swine t-bone kevin bacon. Pig short ribs ground round andouille brisket strip steak. Ball tip filet mignon drumstick doner short loin chuck prosciutto sirloin strip steak burgdoggen chicken shankle kielbasa.",
+                       'drink' => "Fancy Wine"
+                   ),
+                   
+                   "super-salad" => array(
+                       'title' => "The Super Salad<sup>&reg;</sup>",
+                       'price' => 34,
+                       'blurb' => "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+                       'drink' => "Jug o' Water"
+                       
+                   ),
+        
+                   "mexican-barbacoa" => array(
+                       'title' => "Mexican Barbacoa",
+                       'price' => 23    ,
+                       'blurb' => "Bacon ipsum dolor amet cow swine t-bone kevin bacon. Pig short ribs ground round andouille brisket strip steak. Ball tip filet mignon drumstick doner short loin chuck prosciutto sirloin strip steak burgdoggen chicken shankle kielbasa.",
+                       'drink' => "Beer with a lime"
+                       
+                   ),
+               );
 
 ?>
